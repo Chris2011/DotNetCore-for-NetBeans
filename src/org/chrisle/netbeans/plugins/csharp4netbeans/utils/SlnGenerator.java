56b857c071b31e5724ca15542b6a10230d7f8e30
Chris2011 <Christian.Lenz@gmx.net>
2015-07-20 16:29:37 +0200

    Created a class to create a sln file and add projects to it later.

--- src/org/chrisle/netbeans/plugins/csharp4netbeans/utils/SlnGenerator.java
+++ src/org/chrisle/netbeans/plugins/csharp4netbeans/utils/SlnGenerator.java
@@ -3,6 +3,7 @@ package org.chrisle.netbeans.plugins.csharp4netbeans.utils;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import org.chrisle.netbeans.plugins.csharp4netbeans.beans.Sln;
 import org.openide.util.Exceptions;
 
 /**
@@ -10,22 +11,29 @@ import org.openide.util.Exceptions;
  * @author ChrisLE
  */
 public class SlnGenerator {
-    private final String _headInformation;
-
-    public SlnGenerator() {
-        this._headInformation = "\n"+
-                                "Microsoft Visual Studio Solution File, Format Version 12.00\n" +
-                                "# Netbeans\n";
+    private final Sln _sln;
+    
+    public SlnGenerator(Sln sln) {
+        this._sln = sln;
     }
     
-    public void createSlnFile(String projectDirName, String projectFileName) throws IOException {
-        File dir = new File(projectDirName);
-        File slnFile = new File(dir, projectFileName);
+    public void createSlnFile() throws IOException {
+        File dir = new File(this._sln.getProjPath());
+        File slnFile = new File(dir, String.format("{0}.sln", this._sln.getProjName()));
 
         try (FileWriter fileWriter = new FileWriter(slnFile)) {
-            fileWriter.write(this._headInformation);
+            fileWriter.write(_sln.getVersionsHeader());
+            fileWriter.append(_sln.getGlobal());
         } catch (Exception ex) {
             Exceptions.printStackTrace(ex);
         }
+    }
+    
+    public void addProjectToSlnFile(String projectName) {
+        
+    }
+    
+    private void addProjectSettingsToSlnFile(String projectGuid) {
+        
     }
 }
