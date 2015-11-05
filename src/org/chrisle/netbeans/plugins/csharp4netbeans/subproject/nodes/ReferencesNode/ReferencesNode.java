5caa206b20e124c80681462e78cab99c651d2bf5
Chris <chrl@spreadshirt.net>
2015-11-05 16:28:17 +0100

    Added a new ReferencesNode to handle it better.

--- src/org/chrisle/netbeans/plugins/csharp4netbeans/subproject/nodes/ReferencesNode/ReferencesNode.java
+++ src/org/chrisle/netbeans/plugins/csharp4netbeans/subproject/nodes/ReferencesNode/ReferencesNode.java
@@ -1,46 +1,31 @@
 package org.chrisle.netbeans.plugins.csharp4netbeans.subproject.nodes.ReferencesNode;
 
-import java.awt.Image;
-import java.io.IOException;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
-import org.openide.loaders.DataObject;
-import org.openide.loaders.DataObjectNotFoundException;
-import org.openide.nodes.FilterNode;
-import org.openide.util.ImageUtilities;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
 
 /**
  *
  * @author chrl
  */
-public class ReferencesNode extends FilterNode {
+public class ReferencesNode extends AbstractNode {
+
     @StaticResource
     private static final String IMAGE = "org/chrisle/netbeans/plugins/csharp4netbeans/resources/references.png";
 
-    public ReferencesNode(Project proj) throws DataObjectNotFoundException, IOException {
-        super(DataObject.find(proj.getProjectDirectory().getFileObject(".nbReferences") == null ? proj.getProjectDirectory().createFolder(".nbReferences") : proj.getProjectDirectory().getFileObject(".nbReferences")).getNodeDelegate());
-    }
+    private final Project _project;
 
-    @Override
-    public String getDisplayName() {
-        return "References";
-    }
+    public ReferencesNode(Project project) {
+        super(Children.create(new ReferencesChildFactory(), true));
+        setDisplayName("References");
+        setIconBaseWithExtension(IMAGE);
 
-    //Next, we add icons, for the default state, which is
-    //closed, and the opened state; we will make them the same.
-    //
-    //Icons in project logical views are
-    //based on combinations--you can combine the node's own icon
-    //with a distinguishing badge that is merged with it. Here we
-    //first obtain the icon from a data folder, then we add our
-    //badge to it by merging it via a NetBeans API utility method:
-    @Override
-    public Image getIcon(int type) {
-        return ImageUtilities.loadImage(IMAGE);
-    }
+//        XML xml = new XMLDocument(new File("info.xml"));
+//        String xmlString = xml.toString();
+//        System.out.println("XML as String using JCabi library : ");
+//        System.out.println(xmlString);
 
-    @Override
-    public Image getOpenedIcon(int type) {
-        return ImageUtilities.loadImage(IMAGE);
+        this._project = project;
     }
 }
