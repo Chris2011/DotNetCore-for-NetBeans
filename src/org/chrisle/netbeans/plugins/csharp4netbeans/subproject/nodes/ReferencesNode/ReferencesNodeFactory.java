9309e11b86b7f290ae607ca1dd293f884d1e862e
Chris <chrl@spreadshirt.net>
2015-12-03 11:37:06 +0100

    Removed comments.

--- src/org/chrisle/netbeans/plugins/csharp4netbeans/subproject/nodes/ReferencesNode/ReferencesNodeFactory.java
+++ src/org/chrisle/netbeans/plugins/csharp4netbeans/subproject/nodes/ReferencesNode/ReferencesNodeFactory.java
@@ -7,9 +7,6 @@ import org.chrisle.netbeans.plugins.csharp4netbeans.subproject.CSharpSubProject;
 import org.netbeans.api.project.Project;
 import org.netbeans.spi.project.ui.support.NodeFactory;
 import org.netbeans.spi.project.ui.support.NodeList;
-import org.openide.filesystems.FileObject;
-import org.openide.nodes.AbstractNode;
-import org.openide.nodes.Children;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
 
@@ -22,28 +19,9 @@ public class ReferencesNodeFactory implements NodeFactory {
 
     @Override
     public NodeList createNodes(Project project) {
-//        ReferencesNode p = project.getLookup().lookup(ReferencesNode.class);
         CSharpSubProject p = project.getLookup().lookup(CSharpSubProject.class);
         assert p != null;
         return new ReferencesNodeList(p);
-
-        //Optionally, only return a new node
-        //if some item is in the project's lookup:
-        //MyCoolLookupItem item = project.getLookup().lookup(MyCoolLookupItem.class);
-        //if (item != null) {
-//        try {
-//            ReferencesNode nd = new ReferencesNode(project);
-//            return NodeFactorySupport.fixedNodeList(nd);
-//        } catch (DataObjectNotFoundException ex) {
-//            Exceptions.printStackTrace(ex);
-//        } catch (IOException ex) {
-//            Exceptions.printStackTrace(ex);
-//        }
-        //}
-        //If the above try/catch fails, e.g.,
-        //our item isn't in the lookup,
-        //then return an empty list of nodes:
-//        return NodeFactorySupport.fixedNodeList();
     }
 
     private class ReferencesNodeList implements NodeList<Node> {
@@ -56,19 +34,9 @@ public class ReferencesNodeFactory implements NodeFactory {
 
         @Override
         public List<Node> keys() {
-//            FileObject textsFolder = project.getProjectDirectory().getFileObject(project.getProjectDirectory().getName() + ".csproj");            
             List<Node> result = new ArrayList<>();
             
             result.add(new ReferencesNode(project));
-//            if (textsFolder != null) {
-//                for (FileObject textsFolderFile : textsFolder.getChildren()) {
-//                    try {
-//                        result.add(DataObject.find(textsFolderFile).getNodeDelegate());
-//                    } catch (DataObjectNotFoundException ex) {
-//                        Exceptions.printStackTrace(ex);
-//                    }
-//                }
-//            }
             return result;
         }
 
@@ -92,6 +60,5 @@ public class ReferencesNodeFactory implements NodeFactory {
         @Override
         public void removeNotify() {
         }
-
     }
 }
