2a9cfe2bae846effb9c8738f720060d77d692a49
Chris <chrl@spreadshirt.net>
2016-10-20 16:50:04 +0200

    Fixed problem with sln name and folder name not equal.

--- src/org/chrisle/netbeans/plugins/csharp4netbeans/solution/CSharpSolutionFactory.java
+++ src/org/chrisle/netbeans/plugins/csharp4netbeans/solution/CSharpSolutionFactory.java
@@ -1,6 +1,7 @@
 package org.chrisle.netbeans.plugins.csharp4netbeans.solution;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import org.netbeans.api.project.Project;
 import org.netbeans.spi.project.ProjectFactory;
 import org.netbeans.spi.project.ProjectState;
@@ -11,22 +12,35 @@ import org.openide.util.lookup.ServiceProvider;
  *
  * @author ChrisLE
  */
-@ServiceProvider(service=ProjectFactory.class)
+@ServiceProvider(service = ProjectFactory.class)
 public class CSharpSolutionFactory implements ProjectFactory {
 
-    public static final String PROJECT_FILE_EXT = ".sln";
+    public static final String PROJECT_FILE_EXT = "sln";
+    public String _slnFileName = "";
 
     //Specifies when a project is a project, i.e.,
     //if "*.sln" is present in a folder:
     @Override
     public boolean isProject(FileObject projectDirectory) {
-        return projectDirectory.getFileObject(projectDirectory.getName() + PROJECT_FILE_EXT) != null;
+        Enumeration<? extends FileObject> data = projectDirectory.getData(true);
+        
+        while (data.hasMoreElements()) {
+            FileObject nextElement = data.nextElement();
+            
+            if (nextElement.hasExt(PROJECT_FILE_EXT)) {
+                _slnFileName = nextElement.getName().replace(".sln$", "");
+                
+                return projectDirectory.getFileObject(_slnFileName) != null;
+            }
+        }
+
+        return false;
     }
 
     //Specifies when the project will be opened, i.e., if the project exists:
     @Override
     public Project loadProject(FileObject dir, ProjectState state) throws IOException {
-        return isProject(dir) ? new CSharpSolution(dir, state) : null;
+        return isProject(dir) ? new CSharpSolution(dir, state, _slnFileName) : null;
     }
 
     @Override
