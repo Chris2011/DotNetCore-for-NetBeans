80d23391ffae3216dc334e40905e7bc047ba144a
Chris <chrl@spreadshirt.net>
2016-11-01 18:29:56 +0100

    Moved CsSyntaxErrorHighlighter to new java package.

---
+++ src/org/chrisle/netbeans/plugins/csharp4netbeans/filetypes/cs/parser/errorhighlighting/CsSyntaxErrorHightlightingTaskFactory.java
@@ -0,0 +1,17 @@
+package org.chrisle.netbeans.plugins.csharp4netbeans.filetypes.cs.parser.errorhighlighting;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.netbeans.modules.parsing.api.Snapshot;
+import org.netbeans.modules.parsing.spi.TaskFactory;
+
+/**
+ *
+ * @author chrl
+ */
+public class CsSyntaxErrorHightlightingTaskFactory extends TaskFactory {
+    @Override
+    public Collection create (Snapshot snapshot) {
+        return Collections.singleton (new CsSyntaxErrorHighlightingTask());
+    }
+}
