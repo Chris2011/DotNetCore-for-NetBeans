2cf9277f832812a2a3b6e45065d8a8d302d59c8a
Chris <chrl@spreadshirt.net>
2016-11-01 18:30:20 +0100

    Added reformatting.

---
+++ src/org/chrisle/netbeans/plugins/csharp4netbeans/filetypes/cs/parser/reformatting/CsReformatTaskFactory.java
@@ -0,0 +1,18 @@
+package org.chrisle.netbeans.plugins.csharp4netbeans.filetypes.cs.parser.reformatting;
+
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.modules.editor.indent.spi.Context;
+import org.netbeans.modules.editor.indent.spi.ReformatTask;
+
+/**
+ *
+ * @author chrl
+ */
+@MimeRegistration(mimeType="text/x-cs",service=ReformatTask.Factory.class)
+public class CsReformatTaskFactory implements ReformatTask.Factory {
+
+    @Override
+    public ReformatTask createTask(Context context) {
+        return new CsReformatTask(context);
+    }   
+}
